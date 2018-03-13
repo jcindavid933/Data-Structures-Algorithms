833fa21402585d0529643c7c3127eb393bebf6b7
jcindavid933 <36192370+jcindavid933@users.noreply.github.com>
2018-03-12 20:52:14 -0700

    Update CharInt.java

--- Algorithmic Toolbox/Word Count/CharInt.java
+++ Algorithmic Toolbox/Word Count/CharInt.java
@@ -1,59 +1,72 @@
-import java.util.Comparator;
+import java.util.*;
+public class CharInt {
+	// Data fields
+	private char thisChar; // The ASCII character
+	private int  occurrenceCount; // The count of randomChar
 
-public class CharInt implements Comparable<CharInt>, Comparator<CharInt> {
-	// FIELDS
- 	private int count;
-	private char ascii;
-
-	// CONSTRUCTORS
-	public CharInt () {
-		this(0,(char)0);
+	// Default constructor:
+	public CharInt() {
 	}
 
-	public CharInt(int n, char c) {
-		count = n;
-		ascii = c;
+	// Parameterized constructors:
+	public CharInt(char randomChar, int occurrence) {
+		thisChar = randomChar;
+		occurrenceCount = occurrence;
 	}
 
-	// for Comparable interface
-	public int compareTo(CharInt other) {
-		if(count > other.count)
-			return 1;
-		else if (count < other.count)
-			return -1;
-		else { // counts are the same
-			return compare(this, other);
-		}
-	}
-
-	// ACCESSORS
+	// Methods:
+	// Overriding the abstract toString() method
+	@Override // Denotes that we are overriding the default toString() method
 	public String toString() {
-		return ascii +"="+ count;
+		return("" + thisChar + "=" + occurrenceCount);
 	}
 
+	// Accessors / Getters:
 	public char getChar() {
-		return ascii;
+		return (thisChar);
 	}
 
 	public int getCount() {
-		return count;
+		return (occurrenceCount);
 	}
 
-	// only one extra method for the comparator
-	public int compare(CharInt one, CharInt two) {
-		return one.ascii - two.ascii;
+	// Mutators / Setters:
+	public void setChar(char charToSet) {
+		thisChar = charToSet;
 	}
 
-	// MODIFIERS
-	public void setChar(char c) {
-		ascii = c;
+	public void setCount(int occurToSet) {
+		occurrenceCount = occurToSet;
 	}
+}
 
-	public void setCount(int n) {
-		count = n;
+class SortByChar implements Comparator<CharInt> {
+	@Override
+	public int compare(CharInt ci1, CharInt ci2) {
+		// Subtracts two chars from each other
+		return (ci1.getChar() - ci2.getChar()); // Sets the natural order from A to Z
 	}
+}
 
-	public void addOne() {
-		count++;
+class SortByCount implements Comparator<CharInt>{
+	// Lower occurrenceCount goes first
+	@Override
+	public int compare(CharInt ci1, CharInt ci2) {
+		// Subtracts the occurrence counts of the two CharInts.
+		if(ci1.getCount() > ci2.getCount()){
+			return(1); // Greater than
+		}
+		// In the event that two things have the same count:
+		// The lower letter comes first
+		else if (ci1.getCount() == ci2.getCount()) {
+			if (ci1.getChar() > ci2.getChar())
+				return(1); // Greater than if the counts are equal (alphabetical)
+			else {
+				return(-1);
+			}
+		}
+		else {
+			return(-1); // Less than
+		}
 	}
 }
