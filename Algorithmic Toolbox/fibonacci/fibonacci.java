b2985d72d9d714801d6a27e5732fca2f06953ee3
jcindavid933 <36192370+jcindavid933@users.noreply.github.com>
2018-03-13 01:43:58 -0700

    Update fibonacci.java

--- Algorithmic Toolbox/fibonacci/fibonacci.java
+++ Algorithmic Toolbox/fibonacci/fibonacci.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 
 public class Fibonacci {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         int test = 45;
         BigInteger test2 = new BigInteger("45"); // in case of overload
         System.out.println(theBigFib(test)); // a fast recursive version
@@ -10,47 +10,46 @@ public class Fibonacci {
     }
 
     public static int fibonacci(int n) {
-        if (n<=2) {
+    	if (n<=2) {
             return 1;
-        } else {
+        } 
+		else {
             return fibonacci(n-1) + fibonacci(n-2);
         }
     }
 
-// Method that accepts an integer representing the recursion depth
+	// Method that accepts an integer representing the recursion depth
 	public static BigInteger theBigFib(int n) {
 		BigInteger theNumber = BigInteger.valueOf(n);
 		return (theBigFib(theNumber));
 	}
 
-// theBigFib method accepts a BigInteger and performs the initial setup of the Fibonacci sequence
+	// theBigFib method accepts a BigInteger and performs the initial setup of the Fibonacci sequence
 	public static BigInteger theBigFib(BigInteger n) {
 		if (n.intValue() <= 2) {
-// Returns 1 as a BigInteger.
+		// Returns 1 as a BigInteger.
 			BigInteger defaultReturn = new BigInteger("1");
 			return (defaultReturn);
 		}
 		else {
 			BigInteger last = new BigInteger("1");
 			BigInteger last2 = new BigInteger("1");
-// Subtract 2 layers to represent the 2 seed values of the sequence
+			// Subtract 2 layers to represent the 2 seed values of the sequence
 			return(theBigFibHelper(n.intValue()-2, last, last2));
 		}
 	}
 
-// A Helper Method calculates the Fibonacci value.
+	// A Helper Method calculates the Fibonacci value.
 	public static BigInteger theBigFibHelper(int depth, BigInteger last, BigInteger last2) {
-// In the case that the sequence runs out of depth
+		// In the case that the sequence runs out of depth
 		if (depth == 0) {
-// Return the last calculated Fibonacci value
+			// Return the last calculated Fibonacci value
 			return (last);
 		}
-// Get the new current value and the new prior value
+		// Get the new current value and the new prior value
 		BigInteger newLast2 = last;
 		BigInteger current = last.add(last2);
 		return (theBigFibHelper(depth-1, current, newLast2));
 	}
-
 }
-
 // END OF CODE
